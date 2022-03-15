42561c2301523ada9332dc8ad1c868019b8d664c
Nikhil Sharma <nikhil23062000@icloud.com>
2022-03-14 20:35:28 -0400

    added the pseudocode and indepth explanation

--- rotatedBinarySearch/RotatedBinarySearch.java
+++ rotatedBinarySearch/RotatedBinarySearch.java
@@ -1,7 +1,32 @@
 package RotatedBinarySearch;
 
 public class RotatedBinarySearch {
-
+/*
+ * This is used to find the element in the rotated array
+ * 
+ * so the main thing while solving this is 
+ * 
+ * suppose we have the array [3,4,5,1,2]
+ * 
+ * mid is at 2 element 5 
+ * if start<=mid //check if the left side is sorted 
+ * mean it is sorted part now check if the target >=start and <mid
+ * if true then end =mid-1;// means element is in the left part 
+ * else 
+ * start=mid+1;// means element is in the right part 
+ * 
+ * suppose if the left side is not sorted then right side will be sorted for sure so 
+ * no need the check for that thing over here just check 
+ * 
+ * target>mid and <=end 
+ * start=mid+1;
+ * 
+ * other wise it is on the other half
+ * 
+ * 
+ * 
+ * 
+ */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] data = { 1, 0, 1, 1, 1 };
