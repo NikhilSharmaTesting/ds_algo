7ef23444140b34dbbef18da45c64f605919906f7
Nikhil Sharma <nikhil23062000@icloud.com>
2022-03-17 08:43:48 -0400

    added the opps folder

---
+++ Opps/Parent.java
@@ -0,0 +1,13 @@
+package Opps;
+
+public class Parent {
+	
+	protected int a;
+	int b;
+	
+	public Parent(int a, int b) {
+		this.a=a;
+		this.b=b;
+	}
+
+}
---
+++ Opps/TwoClasses.java
@@ -0,0 +1,15 @@
+package Opps;
+
+public class TwoClasses {
+	
+	int a;
+	
+	static class Inner{
+		int b;
+		
+		Inner(int b){
+			this.b=b;
+		}
+	}
+
+}
